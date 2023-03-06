766e0ab19a25d7dd45f51d5524869163e75c9f24
Ibrahim Bashir <ibrahimembash@gmail.com>
2023-03-06 01:27:41 +0100

    modify test

--- pages/header.jsx
+++ pages/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div data-testid="header">Header is on the top</div>
+    <div data-testid="header">Header is on the top first</div>
   )
 }
 
